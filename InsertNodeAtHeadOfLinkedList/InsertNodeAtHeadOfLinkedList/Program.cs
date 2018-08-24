490e6735c26578e561a671dfda5445c0c7ea89fd
Aniket Karmarkar <karmarkaraniket@gmail.com>
2018-08-24 09:21:33 -0500

    added ability to add items to start of a list

---
+++ InsertNodeAtHeadOfLinkedList/InsertNodeAtHeadOfLinkedList/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtHead function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode head, int data)
+    {
+        if (head != null)
+        {
+
+        }
+        else
+        {
+            head = new SinglyLinkedListNode(data);
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++)
+        {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
+            llist.head = llist_head;
+        }
+
+
+
+        PrintSinglyLinkedList(llist->head, "\n", textWriter);
+        textWriter.WriteLine();
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
