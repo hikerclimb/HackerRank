9324fe66de6610ea1925117ac586dc4ab29e1772
Aniket Karmarkar <karmarkaraniket@gmail.com>
2018-08-24 08:57:20 -0500

    added ability to add node to tail end of  list but running into issue because passing head causes problem.

---
+++ InsertNodeAtTailEndOfLinkedList/InsertNodeAtTailEndOfLinkedList/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace InsertNodeAtTailEndOfLinkedList
+{
+    using System.IO;
+    using System;
+
+    public class Solution
+    {
+
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+            }
+
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+        {
+            while (node != null)
+            {
+                textWriter.Write(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    textWriter.Write(sep);
+                }
+            }
+        }
+
+        // Complete the insertNodeAtTail function below.
+
+        /*
+         * For your reference:
+         *
+         * SinglyLinkedListNode {
+         *     int data;
+         *     SinglyLinkedListNode next;
+         * }
+         *
+         */
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        {
+            if (head == null)
+            {
+                head = new SinglyLinkedListNode(data);
+            }
+            else
+            {
+                while (head != null)
+                {
+                    if (head.next == null)
+                    {
+                        head.next = new SinglyLinkedListNode(data);
+                    }
+                    else
+                    {
+                        head = head.next;
+                    }
+                }
+            }
+            return head;
+        }
+
+        static void Main(string[] args)
+        {
+            string output = @"C:\Users\Aniket\Documents\GitHub\HackerRank\InsertNodeAtTailEndOfLinkedList\InsertNodeAtTailEndOfLinkedList";
+
+            TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable(output), true);
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+                llist.head = llist_head;
+
+            }
+
+
+
+            PrintSinglyLinkedList(llist.head, "\n", textWriter);
+            textWriter.WriteLine();
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
