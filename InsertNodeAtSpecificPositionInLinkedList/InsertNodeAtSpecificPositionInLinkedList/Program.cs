b04b464f542ec81d3a3ea025b9aacaf45f73c42f
Aniket Karmarkar <karmarkaraniket@gmail.com>
2018-08-24 13:40:41 -0500

    added ability to insert node but doesn't seem to work on hackerrank why? oh why not?

---
+++ InsertNodeAtSpecificPositionInLinkedList/InsertNodeAtSpecificPositionInLinkedList/Program.cs
@@ -0,0 +1,120 @@
+﻿using System.IO;
+using System;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position)
+    {
+        int counter = 0;
+        SinglyLinkedListNode tmp = null;
+        while (head != null)
+        {
+            if(position - counter == 1)
+            {
+                tmp = head;
+            }
+            if(counter == position)
+            {
+                tmp.next = new SinglyLinkedListNode(data);
+                tmp.next.next = head;
+            }
+            head = head.next;
+            counter++;
+        }
+        return head;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++)
+        {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            llist.InsertNode(llistItem);
+        }
+
+        int data = Convert.ToInt32(Console.ReadLine());
+
+        int position = Convert.ToInt32(Console.ReadLine());
+
+        SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+
+        PrintSinglyLinkedList(llist_head, " ", textWriter);
+        textWriter.WriteLine();
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
