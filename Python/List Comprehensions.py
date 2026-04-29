f8a258e0fa1646a54401402a4aa00ddcf8d386e8
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-04-29 13:16:35 -0500

    looked up solution, I was close

--- Python/List Comprehensions.py
+++ Python/List Comprehensions.py
@@ -1,16 +1,8 @@
-import json
 if __name__ == '__main__':
     x = int(input())
     y = int(input())
     z = int(input())
     n = int(input())
-    out = ''
-    out += '['
-    [out := '[' + str(xnum) + ' , ' + str(ynum) + ' , ' + str(znum) +' ],' +out for xnum in range(0, x+1) for ynum in range(0,y+1) for znum in range(0,z+1) if xnum + ynum + znum != n]
-    out = ''.join(("[",out))
-    out = out[:-2]
-    out += ']'
-    jsonLoad = json.loads(out)
-    jsonLoad = jsonLoad[::-1]
 
-    print(jsonLoad)
+    output = [[xnum,ynum,znum] for xnum in range(x+1) for ynum in range(y+1) for znum in range(z+1) if xnum + ynum + znum != n ]
+    print(output)
