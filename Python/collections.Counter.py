42fbb6aa78e5ca9e491804c8207892f8e65fc611
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-06-10 13:22:02 -0500

    had to lookup how to do problem

--- Python/collections.Counter.py
+++ Python/collections.Counter.py
@@ -1,21 +1,14 @@
 from collections import Counter
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 X = int(input())
-shoeSizeInShop = list(map(int, input().split()))
+counterShoeSizeInShop = Counter(map(int, input().split()))
 N = int(input())
-shoesSizeWantedAndPrice = []
-for size in range(0,N):
-    shoesInfo = (list(map(int, input().split())))
-    shoesSizeWantedAndPrice.append(tuple(shoesInfo))
-counterOfShoeSizeAndPrice = Counter(shoesSizeWantedAndPrice)
+
 summ = 0
-for item, frequency in counterOfShoeSizeAndPrice.items():
-    #print('i:'+str(i))
-    for i in range(0, X):
-        if item[0] == shoeSizeInShop[i]:
-            #print(str(item[0]) + ':' + str(item[1]))
-            #print(shoeSizeInShop[i])
-            summ += item[1]
-            break
+for i in range(0,N):
+    shoeSize, price = map(int, input().split())
+    if counterShoeSizeInShop[shoeSize] > 0:
+        summ += price
+        counterShoeSizeInShop[shoeSize] -= 1
+        
 print(summ)
