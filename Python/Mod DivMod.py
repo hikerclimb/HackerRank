cf2eb85f85fc9b0ffcf2a33e1f37b14d72aa450c
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-06-22 09:41:46 -0500

    got one more working :)

---
+++ Python/Mod DivMod.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b= int(input())
+
+out1 = a//b
+out2 = a%b
+out3 = divmod(a,b)
+
+print(out1)
+print(out2)
+print(out3)
