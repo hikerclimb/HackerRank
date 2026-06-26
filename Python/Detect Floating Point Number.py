111e7f15c4defc2c6e886b061c68e2fb9f764384
Aniket Karmarkar <karmarkaraniket@gmail.com>
2026-06-26 09:18:43 -0500

    regex was wrong

--- Python/Detect Floating Point Number.py
+++ Python/Detect Floating Point Number.py
@@ -3,14 +3,14 @@ T = int(input())
 TList = [input() for _ in range(T)]  
 for i in TList:
     try:
-        mat = bool(re.match(r"[-\+\d\.]*\d.*", i))
-        print(mat)
+        mat = bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$', i))
+        #print(mat)
         #print(mat)
         if mat is True:
             print('True')
         else:
             print('False')
     except:
-        print('enter')
-        #print('False')
+        #print('enter')
+        print('False')
         
